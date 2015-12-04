00d0fae02156d347ed64818531976a9b35e83995
DengYun <tdzl2003>
2015-12-04 23:42:27 +0800

    support google analytics

--- src/redux/middleware/transitionMiddleware.js
+++ src/redux/middleware/transitionMiddleware.js
@@ -8,6 +8,16 @@ import {startFetchData, fetchDataOver, fetchDataFailed} from '../modules/fetchDa
 
 const locationsAreEqual = (locA, locB) => locA && locB && (locA.pathname === locB.pathname) && (locA.search === locB.search);
 
+if (__CLIENT__ && !__DEV__){
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-63485149-4', 'auto');
+  ga('send', 'pageview');
+}
+
 export default ({getState, dispatch}) => next => action => {
   if (action.type === ROUTER_DID_CHANGE) {
     if (getState().router && locationsAreEqual(action.payload.location, getState().router.location)) {
@@ -27,6 +37,9 @@ export default ({getState, dispatch}) => next => action => {
       .then(()=>Promise.all(getDataDependencies(components, getState, dispatch, location, params)))
       .then(() => {
         next(action);
+        if (__CLIENT__ && !__DEV__){
+          ga('send', 'pageview');
+        }
         return Promise.all(getDataDependencies(components, getState, dispatch, location, params, true));
       })
       .then(()=>dispatch(fetchDataOver()), err=>{
