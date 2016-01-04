2937f4f550048e30725715b6ae20e9c8968126f7
sunnylqm <sunnylqm@qq.com>
2016-01-04 13:50:55 +0800

    keyword => keywords

--- src/options.js
+++ src/options.js
@@ -9,7 +9,7 @@ module.exports = Object.assign({
     meta: {
       charSet: 'utf-8',
       property: {
-        'keyword': 'react native,react native中文网,react native ios,react native android,react native教程',
+        'keywords': 'react native,react native中文网,react native ios,react native android,react native教程',
         'og:site_name': 'React Native 中文网',
         'og:image': 'https://react-native.cn/assets/img/opengraph.png',
         'og:locale': 'zh_CN',
@@ -17,7 +17,7 @@ module.exports = Object.assign({
         'og:description': 'React Native中文网是中国最大的React Native开发者交流学习平台,致力于打造React Native开发。在这里能轻松找到代码实例,项目案例,并有专人提供最新文档翻译.',
       },
       name: {
-        'keyword': 'react native,react native中文网,react native ios,react native android,react native教程',
+        'keywords': 'react native,react native中文网,react native ios,react native android,react native教程',
         'description': 'React Native中文网是中国最大的React Native开发者交流学习平台,致力于打造React Native开发。在这里能轻松找到代码实例,项目案例,并有专人提供最新文档翻译.',
       }
     }
