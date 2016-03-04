ebf45e2106c7a2ff932a9a881f6ba9547d2c0d1a
DengYun <tdzl2003@gmail.com>
2016-03-04 11:02:22 +0800

    Update MyNavBar.js

--- src/components/MyNavBar.js
+++ src/components/MyNavBar.js
@@ -21,7 +21,7 @@ const linksInternal = [
   { section: 'cases', href: '/cases.html', text: '案例'},
   //{ section: 'blog', href: '/blog.html', text: '博客' },
   { section: 'bbs', href: 'http://bbs.reactnative.cn/', text: '讨论', hot: true, newTab: false },
-  { section: 'gzfx', hot: true, href: 'http://bbs.reactnative.cn/topic/321/', text: '广州分享会', newTab: true },
+  { section: 'shfx', hot: true, href: 'http://bbs.reactnative.cn/topic/509', text: '3.19上海分享会', newTab: true },
   { section: 'about', href: '/about.html', text: '关于', hash: '#content' },
 ];
 const linksExternal = [
