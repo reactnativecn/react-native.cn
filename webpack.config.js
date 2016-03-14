098eced0bf8465b6f8c0310608a58fb2bf8ec5f3
DengYun <tdzl2003@gmail.com>
2016-03-14 11:45:45 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -63,7 +63,6 @@ module.exports = {
   },
   module: {
     preLoaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'eslint'},
     ],
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loaders: ['babel?' + JSON.stringify(babelLoaderQuery)]},
