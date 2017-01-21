9ca14836a496b068f5c13d943320cbc363269dc7
sunnylqm <sunnylqm@qq.com>
2017-01-21 22:37:35 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ export default function Footer() {
       <div className="container">
         <div className="right">
           <p>React Native中文网.</p>
-          <p>© 2016 杭州欧石南网络科技有限公司</p>
+          <p>© 2017 杭州欧石南网络科技有限公司</p>
           <p className="gray">浙ICP备15023664号-3</p>
           <p className="gray">
             <img src={imageGhs} alt="浙公网安备 33010602005511号" />
