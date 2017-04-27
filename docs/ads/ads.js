5fd3e03d7cc2e6adc783056401ee7921b89e205d
sunnylqm <sunnylqm@qq.com>
2017-04-27 13:58:05 +0800

    Update ads.js

--- docs/ads/ads.js
+++ docs/ads/ads.js
@@ -1,19 +1,18 @@
 export default {
   index: {
     banner: {
-      img: require('./img/classmates.png'),
+      img: require('./img/course01.png'),
       text: '听晴明老师从头讲React Native',
-      link: 'https://m.ke.qq.com/courseShare.html?_bid=167&_wv=1819&cid=197101&actid=11640&tid=100233637#from=sharetool',
-      gainfo: 'classmates/index'
+      link: 'https://ke.qq.com/course/197101',
+      gainfo: 'course01/index'
     }
   },
   docs: {
     banner: {
-      img: require('./img/classmates.png'),
+      img: require('./img/course01.png'),
       text: '听晴明老师从头讲React Native',
-      link: 'https://m.ke.qq.com/courseShare.html?_bid=167&_wv=1819&cid=197101&actid=11640&tid=100233637#from=sharetool',
-      gainfo: 'classmates/docs'
+      link: 'https://ke.qq.com/course/197101',
+      gainfo: 'course01/docs'
     }
   }
-
 }
