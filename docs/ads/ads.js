8beece970f2f53d50f6d68b74d0b037e4e73a9f6
sunnylqm <sunnylqm@qq.com>
2017-04-28 08:54:40 +0800

    Update ads.js

--- docs/ads/ads.js
+++ docs/ads/ads.js
@@ -3,16 +3,16 @@ export default {
     banner: {
       img: require('./img/course01.png'),
       text: '听晴明老师从头讲React Native',
-      link: '/post/3376',
-      gainfo: 'post3376/index'
+      link: '/post/3798',
+      gainfo: 'post3798/index'
     }
   },
   docs: {
     banner: {
       img: require('./img/course01.png'),
       text: '听晴明老师从头讲React Native',
-      link: '/post/3376',
-      gainfo: 'post3376/docs'
+      link: '/post/3798',
+      gainfo: 'post3798/docs'
     }
   }
 }
